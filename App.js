2f74db08fb02c2549ab9873033b60f5f05dad6fb
MiguelAngelVenturaMarin <mig3l_angel@hotmail.es>
2018-11-28 20:17:32 -0600

    Login con facebook desde react native -> configuración de android para clases de fb y llamado al boton de login desde el componente Nota: modificar ruta de api fb en strings.xml

---
+++ App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+var FBLoginButton = require('./components/FBLoginButton');
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginManager,
+} = FBSDK;
+    
+// ...
+
+// Attempt a login using the Facebook login dialog,
+// asking for default permissions.
+LoginManager.logInWithReadPermissions(['public_profile']).then(
+  function(result) {
+    if (result.isCancelled) {
+      alert('Login was cancelled');
+    } else {
+      alert('Login was successful with permissions: '
+        + result.grantedPermissions.toString());
+    }
+  },
+  function(error) {
+    alert('Login failed with error: ' + error);
+  }
+);
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <FBLoginButton />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
